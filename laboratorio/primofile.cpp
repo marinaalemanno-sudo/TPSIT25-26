3d52666d76818573f8d1f123f1e12597edeb0aa9
marinaalemanno-sudo <marina.alemanno@salveminialessano.edu.it>
2025-09-18 09:51:03 +0200

    Create primofile.cpp

---
+++ laboratorio/primofile.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string nome, cognome;
+    nome = "Marina";
+        cognome = "Alemanno";
+   cout << "Ciao " << nome << " "<< cognome;
+    return 0;
+}
