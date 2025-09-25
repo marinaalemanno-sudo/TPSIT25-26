20751ae599be848ce4d7475677342db87be65a93
marinaalemanno-sudo <marina.alemanno@salveminialessano.edu.it>
2025-09-25 13:01:35 +0200

    Update uno.cpp
    agiunta sottrazione

--- laboratorio/uno.cpp
+++ laboratorio/uno.cpp
@@ -5,7 +5,14 @@ int main()
 	int num1;
 	int mun2;
 	int somma;
+	int sottrazione;
+	
 	int somma=num1 + num2;
-	cout<<"la somma nei numeri e "<<somma;
+	cout<<"la somma dei numeri e "<<somma;
+	
+	
+	int sottrazione=num1 - num2;
+	cout<<"la sottrazione dei numeri e "<<sottrazione;
+	
 	return 0;
 }
