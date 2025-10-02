9db9822fcb8005eae79b282228affff23a3d39da
marinaalemanno-sudo <marina.alemanno@salveminialessano.edu.it>
2025-10-02 11:36:26 +0200

    Delete laboratorio/primofile.cpp

--- laboratorio/primofile.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    string nome, cognome;
-    nome = "Marina";
-        cognome = "Alemanno";
-   cout << "Ciao " << nome << " "<< cognome;
-    return 0;
-}
